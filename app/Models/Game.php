5ffa7beadd51fc1ef65e98c8eecfbf8864485124
Javier <jponciang@gmail.com>
2026-05-13 15:33:32 -0400

    feat: create Game model with lock logic and team name translation attributes

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -44,4 +44,60 @@ class Game extends Model
     public function predictions()
     {
         return $this->hasMany(Prediction::class);
-    }}
+    }
+
+    private function translateTeam($team)
+    {
+        $translations = [
+            'Mexico' => 'México',
+            'South Korea' => 'Corea del Sur',
+            'Canada' => 'Canadá',
+            'United States' => 'Estados Unidos',
+            'Qatar' => 'Catar',
+            'Brazil' => 'Brasil',
+            'Haiti' => 'Haití',
+            'Germany' => 'Alemania',
+            'Netherlands' => 'Países Bajos',
+            'Ivory Coast' => 'Costa de Marfil',
+            'Sweden' => 'Suecia',
+            'Spain' => 'España',
+            'Belgium' => 'Bélgica',
+            'Saudi Arabia' => 'Arabia Saudita',
+            'Iran' => 'Irán',
+            'France' => 'Francia',
+            'Iraq' => 'Irak',
+            'Uzbekistan' => 'Uzbekistán',
+            'Czechia' => 'República Checa',
+            'Switzerland' => 'Suiza',
+            'Scotland' => 'Escocia',
+            'Turkey' => 'Turquía',
+            'Tunisia' => 'Túnez',
+            'New Zealand' => 'Nueva Zelanda',
+            'Norway' => 'Noruega',
+            'Jordan' => 'Jordania',
+            'Panama' => 'Panamá',
+            'Bosnia-Herzegovina' => 'Bosnia y Herzegovina',
+            'Morocco' => 'Marruecos',
+            'South Africa' => 'Sudáfrica',
+            'Curaçao' => 'Curazao',
+            'Japan' => 'Japón',
+            'Cape Verde Islands' => 'Cabo Verde',
+            'Egypt' => 'Egipto',
+            'Croatia' => 'Croacia',
+            'Congo DR' => 'R.D. del Congo',
+            'Algeria' => 'Argelia'
+        ];
+
+        return $translations[$team] ?? $team;
+    }
+
+    public function getTeamAAttribute($value)
+    {
+        return $this->translateTeam($value);
+    }
+
+    public function getTeamBAttribute($value)
+    {
+        return $this->translateTeam($value);
+    }
+}
